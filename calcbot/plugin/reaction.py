516c219f6422c2e3b37a1d70c2ea11c30789ae1e
mkohei <baberugaberiimeronnv5@gmail.com>
2017-03-06 06:57:00 +0900

    Calc ver1.1
    Syntax error による例外処理を追加

--- calcbot/plugin/reaction.py
+++ calcbot/plugin/reaction.py
@@ -64,16 +64,19 @@ def reaction_calc(message):
 def reaction_calc(message):
 	expr = message.body[u'text']
 	if expr_matcher.fullmatch(expr):
-		result = expression.parseString(expr)
-		#print(result, type(result), result[0], type(result[0]))
-		val = result[0]
-		if isinstance(val, str):
-			val = int(val)
-		else:
-			val = ast.calc(val)
-		#print(val)
-		#message.reply(str(result))
-		message.reply(str(val))
+		try:
+			result = expression.parseString(expr)
+			#print(result, type(result), result[0], type(result[0]))
+			val = result[0]
+			if isinstance(val, str):
+				val = int(val)
+			else:
+				val = ast.calc(val)
+			#print(val)
+			#message.reply(str(result))
+			message.reply(str(val))
+		except:
+			message.reply("throw exception. maybe syntax error.")
 	else:
 		message.reply(DEFAULT_REPLY)
 
